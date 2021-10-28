8fb6994b98dc629286e017a90a73e0c4c9dbdd11
Carla <carlalopezprogramacion@gmail.com>
2021-10-27 21:26:28 -0300

    cambios menores

--- js/boton.js
+++ js/boton.js
@@ -2,7 +2,7 @@ const iconoMenu = document.getElementById("icono-menu");
 const iconoMenuActivo = document.getElementById("icono-menu-activo");
 const menu = document.getElementById("menu");
 iconoMenu.addEventListener("click",()=>{
-    menu.style.top = "0px";
+    menu.style.top = "-10vh";
 })
 
 iconoMenuActivo.addEventListener("click",()=>{
